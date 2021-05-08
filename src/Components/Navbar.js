c35b16cce4f74a07265804d807fc66c116335bda
Farhod <farhodxolxojayev2199@gmail.ru>
2021-05-08 18:32:30 +0500

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
